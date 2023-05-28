820b1af2d08a2360f9c26fdd19b291a619dd51d6
mikus3363 <273024@student.pwr.edu.pl>
2023-05-28 19:18:19 +0200

    test

--- src/main/java/me/simulation/ApplicationMain.java
+++ src/main/java/me/simulation/ApplicationMain.java
@@ -5,9 +5,12 @@ import me.simulation.equipments.*;
 import me.simulation.game.Map;
 import me.simulation.players.*;
 
-
+//test git
 public class ApplicationMain {
     public static void main(String[] args) {
-
     }
 }
+class Test{
+
+}
+
