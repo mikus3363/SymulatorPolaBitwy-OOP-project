ff6b0b46eb23648fdb2dc2efb2e1843e984cc596
mikus3363 <273024@student.pwr.edu.pl>
2023-05-29 17:33:24 +0200

    mapa

--- src/main/java/me/simulation/ApplicationMain.java
+++ src/main/java/me/simulation/ApplicationMain.java
@@ -8,9 +8,9 @@ import me.simulation.players.*;
 //test git
 public class ApplicationMain {
     public static void main(String[] args) {
-        Ork ork1 = new Ork("ork", 5,20,10,1,0,false,false,2,3);
-        Human human1 = new Human("human", 5,20,10,1,0,false,false,2,3);
-        Elf elf1 = new Elf("elf", 5,20,10,1,0,false,false,2,3);
+        Ork ork1 = new Ork("ork", 5,20,10,1,0,false,false,2,8);
+        Human human1 = new Human("human", 5,20,10,1,0,false,false,0,0);
+        Elf elf1 = new Elf("elf", 5,20,10,1,0,false,false,1,2);
         ArrayList<ArrayList<Champion>> mapa = new ArrayList<>(12);
 
 
@@ -21,11 +21,11 @@ public class ApplicationMain {
             }
         }
 
-        mapa.get(0).add(0, ork1);
-        mapa.get(0).add(1, human1);
-        mapa.get(0).add(9, elf1);
+        mapa.get(ork1.x_index).add(ork1.y_index, ork1);
+        mapa.get(human1.x_index).add(human1.y_index, human1);
+        mapa.get(elf1.x_index).add(elf1.y_index, elf1);
 
-        mapa.get(8).add(3, ork1);
+        mapa.get(0).add(0, ork1); //gdy 2 champions są na jednym polu ten który był tam pierwszy przesuwa się o jedno pole w prawo, potyrzebne warunki
         mapa.get(2).add(4, human1);
         mapa.get(6).add(8, elf1);
 
@@ -37,41 +37,7 @@ public class ApplicationMain {
         mapa.get(2).add(7, human1);
         mapa.get(5).add(6, elf1);
 
-        System.out.print("_");
-        for (int i = 0 ; i < 12 ; i++ ) {
-            System.out.print("___");
-        }
-        System.out.print("_");
-        System.out.println();
-
-        for (int i = 0 ; i < 12 ; i++ ) {
-            System.out.print("|");
-            for (int j = 0 ; j < 12 ; j++ ) {
-                if(mapa.get(i).get(j) != null){
-                    if(mapa.get(i).get(j).race == "ork"){
-                        System.out.print(" + ");
-                    }
-                    if(mapa.get(i).get(j).race == "human"){
-                        System.out.print(" * ");
-                    }
-                    if(mapa.get(i).get(j).race == "elf"){
-                        System.out.print(" - ");
-                    }
-                }
-                else {
-                    System.out.print("   ");
-                }
-            }
-            System.out.print("|");
-            System.out.print("\n");
-        }
-        System.out.print("_");
-        for (int i = 0 ; i < 12 ; i++ ) {
-            System.out.print("___");
-        }
-        System.out.print("_");
-        System.out.println();
-
+        new Map(mapa);
     }
 }
 
--- src/main/java/me/simulation/game/Map.java
+++ src/main/java/me/simulation/game/Map.java
@@ -1,5 +1,9 @@
 package me.simulation.game;
 
+import me.simulation.players.Champion;
+
+import java.util.ArrayList;
+
 public class Map {
     public int width;
     public int height;
@@ -7,6 +11,42 @@ public class Map {
     public Map(int width, int height){
         this.height = height;
         this.width = width;
+    }
+    public Map(ArrayList<ArrayList<Champion>> mapa){
+        System.out.print("_");
+        for (int i = 0 ; i < 12 ; i++ ) {
+            System.out.print("___");
+        }
+        System.out.print("_");
+        System.out.println();
+
+        for (int i = 0 ; i < 12 ; i++ ) {
+            System.out.print("|");
+            for (int j = 0 ; j < 12 ; j++ ) {
+                if(mapa.get(i).get(j) != null){
+                    if(mapa.get(i).get(j).race == "ork"){
+                        System.out.print(" O ");
+                    }
+                    if(mapa.get(i).get(j).race == "human"){
+                        System.out.print(" H ");
+                    }
+                    if(mapa.get(i).get(j).race == "elf"){
+                        System.out.print(" E ");
+                    }
+                }
+                else {
+                    System.out.print("   ");
+                }
+            }
+            System.out.print("|");
+            System.out.print("\n");
+        }
+        System.out.print("_");
+        for (int i = 0 ; i < 12 ; i++ ) {
+            System.out.print("___");
+        }
+        System.out.print("_");
+        System.out.println();
     }
 
     public static void clear(){
