e45ac0368d366c869d8fcd647005d4df33c0e8c1
mikus3363 <273024@student.pwr.edu.pl>
2023-05-30 20:24:54 +0200

    mapa

--- src/main/java/me/simulation/ApplicationMain.java
+++ src/main/java/me/simulation/ApplicationMain.java
@@ -25,48 +25,61 @@ public class ApplicationMain {
                         list.get(i).get(j).move = false;
                         Random generator = new Random();
                         Champion champ = list.get(i).get(j);
-                        list.get(i).remove(j);
-                        list.get(i).add(j,null);
                         int randommove = generator.nextInt(4);//zawsze sie rusza, warunki mało zdrowia zawsze 0
                         switch (randommove){
                             case 0://stój w miejscu
                                 break;
                             case 1://ruch w prawo
                                 if(j + 1 > 10){//ściana
+
                                     list.get(i).remove(j - 1);
                                     list.get(i).add(j - 1,champ);
                                 }
                                 else {
+                                    list.get(i).remove(j);
+                                    list.get(i).add(j, null);
                                     list.get(i).remove(j + 1);
                                     list.get(i).add(j + 1, champ);
                                 }
                                 break;
                             case 2://ruch w lewo
                                 if(j - 1 < 0){//ściana
+                                    list.get(i).remove(j);
+                                    list.get(i).add(j,null);
                                     list.get(i).remove(j + 1);
                                     list.get(i).add(j + 1,champ);
                                 }
                                 else {
+                                    list.get(i).remove(j);
+                                    list.get(i).add(j, null);
                                     list.get(i).remove(j - 1);
                                     list.get(i).add(j - 1, champ);
                                 }
                                 break;
                             case 3://ruch do góry
                                 if(i - 1 < 0){//ściana
+                                    list.get(i).remove(j);
+                                    list.get(i).add(j,null);
                                     list.get(i + 1).remove(j);
                                     list.get(i + 1).add(j,champ);
                                 }
                                 else {
+                                    list.get(i).remove(j);
+                                    list.get(i).add(j, null);
                                     list.get(i - 1).remove(j);
                                     list.get(i - 1).add(j, champ);
                                 }
                                 break;
                             case 4://ruch w dół
                                 if(i + 1 > 10){//ściana
+                                    list.get(i).remove(j);
+                                    list.get(i).add(j,null);
                                     list.get(i - 1).remove(j);
                                     list.get(i - 1).add(j,champ);
                                 }
                                 else {
+                                    list.get(i).remove(j);
+                                    list.get(i).add(j, null);
                                     list.get(i + 1).remove(j);
                                     list.get(i + 1).add(j, champ);
                                 }
